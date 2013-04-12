015177b0e3933c186635736f526d2f961cbb5362
Justin Wood <justin.k.wood@gmail.com>
2013-04-12 16:50:37 -0400

    generate random token, store it in a cookie

--- remote_logger.js
+++ remote_logger.js
@@ -6,16 +6,39 @@ var RemoteLogger = {};
     self.host = window.location.hostname;
     self.useragent = navigator.userAgent;
     self.path = window.location.pathname;
+    self.token = $.cookie('remote_logger_token') || generate_token();
+
+    var date = new Date();
+    date.setTime(date.getTime() + 60 * 60 * 1000);
+    $.cookie('remote_logger_token', self.token, {path: '/', expires: date});
 
     self.log = function(message) {
         console.log(message);
         var log = {message: message, host: self.host, path: self.path,
-                   useragent: self.useragent};
-        $.post('http://localhost/phpcorsproxy.php/logger/api/log/create.json',
+                   useragent: self.useragent, token: self.token};
+        $.post('http://localhost/phpcorsproxy.php/logger/api/log/create.Jason',
                {log: log},
                function(data) {
                    // do something once the request returns
                    console.log(data);
                });
     };
+
 })(RemoteLogger);
+
+function generate_token() {
+    // 10 characters
+    // alpha numeric
+    var pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var result = "";
+    var tmp;
+    var number;
+
+    for (var i=0;i < 10; i++) {
+        number = Math.floor(Math.random() * (pool.length));
+        tmp = pool.substring(number,number + 1);
+        result += tmp;
+    }
+
+    return result;
+};
